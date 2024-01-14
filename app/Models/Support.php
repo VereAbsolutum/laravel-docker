374f31351436d6b455dc2199dd925ef055513637
sergio <sergiopile3@gmail.com>
2024-01-14 19:14:16 -0300

    criado: 1. criado uuid na tabela do banco (support, user) 2. criado Observer para cadastro do uuid do user autenticado na tabela support, user_id, quando cadastrado uma nova resposta (replies)

--- app/Models/Support.php
+++ app/Models/Support.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use App\Enums\SupportStatus;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Support extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUlids;
 
     protected $fillable = [
         'subject',
